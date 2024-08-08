571dd8aa4acc42117912dd18203e0a60b5ac7be5
eosthehub <machadocpatricia4@gmail.com>
2024-08-07 22:35:32 -0300

    Update animacao.js

--- animacao.js
+++ animacao.js
@@ -1,15 +1,85 @@
-document.getElementById('powerButton').addEventListener('click', function() {
-    var animation = document.getElementById('animation');
-    var scenes = document.getElementsByClassName('scene');
-    if (animation.style.display === 'none' || animation.style.display === '') {
-        animation.style.display = 'block';
-        for (var i = 0; i < scenes.length; i++) {
-            scenes[i].style.display = 'block';
-        }
+//variaveis de contagem da direção
+
+var num = 300;
+var num2 = 300;
+
+//slide do botão direito
+
+var btnRight = document.getElementById('btnright');
+
+btnRight.addEventListener('click', () => {
+
+    var limi = document.getElementById('limitador').getBoundingClientRect().left;
+
+    var limit = Math.round(limi);
+
+    var er = document.getElementById('btnright').getBoundingClientRect().left;
+
+    var ref = Math.round(er);
+
+    var slideContainer = document.getElementById('container');
+
+    var carrossel = document.getElementById('carrossel');
+
+
+    if (limit > ref) {
+
+        slideContainer.style.transform = 'translateX(-' + num + 'px)';
+
+        num = num + 300;
+
+        num2 = num2 - 300;
+
     } else {
-        animation.style.display = 'none';
-        for (var i = 0; i < scenes.length; i++) {
-            scenes[i].style.display = 'none';
-        }
+
+        var stoped = ref * slideContainer.clientWidth / carrossel.clientWidth - carrossel.clientWidth + 100;
+
+        var stopedRound = Mtah.round(stoped);
+
+        slideContainer.style.transform = 'translateX(-' + stopedRound + 'px)';
+
+
     }
+
+
+});
+
+
+//slide botão esquerdo
+
+var btnLeft = document.getElementById('btnleft');
+
+btnLeft.addEventListener('click', () => {
+
+
+    var limi = document.getElementById('limitador').getBoundingClientRect().left;
+
+    var limit = Math.round(limi);
+
+    var er = document.getElementById('btnleft').getBoundingClientRect().left;
+
+    var ref = Math.round(er);
+
+    var slideContainer = document.getElementById('container');
+
+    var carrossel = document.getElementById('carrossel');
+
+    var slide = slideContainer.getBoundingClientRect().left;
+
+    if (slide < ref) {
+
+        slideContainer.style.transform = 'translateX(' + num2 + 'px)';
+
+        num = num - 300;
+        num2 = num2 + 300;
+
+    } else {
+
+        slideContainer.style.transform = 'translateX(0)';
+
+        num = 300;
+
+    }
+
+
 });
