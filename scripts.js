eb97bc3884c016148ce8d850e5c1b6a6f9fb231d
eosthehub <machadocpatricia4@gmail.com>
2024-08-26 14:16:57 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,3 +29,7 @@ function checkPassword() {
         errorMessage.style.display = 'block';
     }
 }
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("active");
+});
