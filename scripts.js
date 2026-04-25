291ca49ad6a5fbdf3cd6a9e426644a8b83d4ed46
eosthehub <machadocpatricia4@gmail.com>
2026-04-25 09:59:43 -0300

    Atualizar o scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,4 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-content");
-
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
 }
-
-/* Fecha o menu se clicar fora */
-document.addEventListener("click", function(event) {
-    const menu = document.querySelector(".menu-content");
-    const button = document.querySelector(".menu-button");
-
-    if (!button.contains(event.target) && !menu.contains(event.target)) {
-        menu.style.display = "none";
-    }
-});
